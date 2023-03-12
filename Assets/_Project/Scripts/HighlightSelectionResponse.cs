b04cddc5329ecdf2d29ae6d277f120aed38692a8
DenisPanfilov0 <denispanfilov119@gmail.com>
2023-03-12 22:24:50 +0300

    Refactoring

--- Assets/_Project/Scripts/HighlightSelectionResponse.cs
+++ Assets/_Project/Scripts/HighlightSelectionResponse.cs
@@ -5,21 +5,21 @@ internal class HighlightSelectionResponse : MonoBehaviour, ISelectionResponse
     [SerializeField] public Material highlightMaterial;
     [SerializeField] public Material defaultMaterial;
 
-    public void OnSelect(Transform selection)
-    {
-        var selectionRenderer = selection.GetComponent<Renderer>();
-        if (selectionRenderer != null)
-        {
-            selectionRenderer.material = this.highlightMaterial;
-        }
-    }
-
     public void OnDeselect(Transform selection)
     {
         var selectionRenderer = selection.GetComponent<Renderer>();
         if (selectionRenderer != null)
         {
-            selectionRenderer.material = this.defaultMaterial;
+            selectionRenderer.material = defaultMaterial;
+        }
+    }
+
+    public void OnSelect(Transform selection)
+    {
+        var selectionRenderer = selection.GetComponent<Renderer>();
+        if (selectionRenderer != null)
+        {
+            selectionRenderer.material = highlightMaterial;
         }
     }
 }
--- Assets/_Project/Scripts/OutlineSelectionResponse.cs
+++ Assets/_Project/Scripts/OutlineSelectionResponse.cs
@@ -5,12 +5,18 @@ public class OutlineSelectionResponse : MonoBehaviour, ISelectionResponse
     public void OnSelect(Transform selection)
     {
         var outline = selection.GetComponent<Outline>();
-        if (outline != null) outline.OutlineWidth = 10;
+        if (outline != null)
+        {
+            outline.OutlineWidth = 10;
+        }
     }
 
     public void OnDeselect(Transform selection)
     {
         var outline = selection.GetComponent<Outline>();
-        if (outline != null) outline.OutlineWidth = 0;
+        if (outline != null)
+        {
+            outline.OutlineWidth = 0;
+        }
     }
 }
